1ae0bb4b24bd9dddb37450b09054cd28426436d3
Venght <bvenght@gmail.com>
2025-01-28 03:23:22 +0800

    The panlindromes 09 is done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const newString = string
+    .toLowerCase()
+    .split('')
+    .filter((index) => alphanumerical.includes(index))
+    .join('');
+
+    const reverseString = newString.split('').reverse().join('');
+
+    return reverseString == newString;
 };
 
 // Do not edit below this line
